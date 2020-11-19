070f9ef308d0ab84f113e25d498737e774e083e9
Rzzzzzj <63907441+Rzzzzzj@users.noreply.github.com>
2020-11-19 18:02:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+
+bot.on('ready', () => {
+    console.log("I'm ready!")
+})
+
+bot.on('message', msg => {
+    if(msg.content == "TT") {
+        msg.channel.send("TT is here!")
+    }
+    
+})
+
+bot.on('message', msg => {
+    if(msg.content == "question") {
+        msg.reply("If you have any questions, please go to the question channel to ask, and the administrator will answer you for you after the them are online, thank you!")
+    }
+    
+})
+
+bot.on('message', msg => {
+    if(msg.content == "hello") {
+        msg.channel.send("hi")
+    }
+    
+})
+
+
+bot.login("Nzc4ODk2NTU1NTc0Mjk2NTc2.X7Yp8g.maFE1Yrw2EzX6QFEWQTTD7U-RjQ")
